0508c900c683d73eb6cb6bd579d552e0b284fdeb
RubenCardozo <cardozoruben@gmail.com>
2018-01-29 16:03:13 +0100

    Premier programme JS

---
+++ ITTAJS/ts/debut.js
@@ -0,0 +1,18 @@
+"use strict";
+var monVariable = "Hello world";
+console.log(monVariable);
+var ma = "Blanca!!!";
+console.log(ma);
+var a;
+console.log(a);
+///
+b = 12;
+console.log(b);
+var b;
+console.log(typeof (b));
+console.log(typeof (a));
+var c = true;
+console.log(typeof (c));
+var c = 1;
+console.log(typeof (c));
+console.trace(typeof (c));
