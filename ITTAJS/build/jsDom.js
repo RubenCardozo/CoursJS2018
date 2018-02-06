c72bfba1627d16424373cb97160f702de62480eb
RubenCardozo <cardozoruben@gmail.com>
2018-02-06 16:32:53 +0100

    Selecteurs js

--- ITTAJS/build/jsDom.js
+++ ITTAJS/build/jsDom.js
@@ -80,7 +80,74 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_jquery___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_jquery__);
 
 
-document.getElementById("premiere").innerHTML="Premiere";
+window.onload=function(){
+    document.getElementById("premiere").innerHTML="<b>Premiere</b>";
+    document.getElementById("premiere").innerText="<b>Premiere</b>";
+
+    console.log(document.getElementsByTagName("div"));
+
+    var divs = document.getElementsByTagName("div");
+    divs[1].innerHTML = "deuxieme";
+
+    for (var index = 0; index < divs.length; index++){
+        divs[index].innerHTML="div " + index;
+        console.log("+");  
+    }
+    // for (var index  in  divs) {
+    //     divs[index].innerHTML="div-" + index;
+    //     console.log("-");  
+    // } A eviter
+   
+    var tdiv=Array.prototype.slice.call(divs,0);
+    for (var index  in  tdiv) {
+            divs[index].innerHTML="div-" + index;
+            console.log("-");
+    }
+    
+    tdiv.forEach(function(element, index){
+        element.innerHTML="div +"+index;
+    });
+
+    console.log(document.getElementsByClassName('ladiv'));
+    console.log(document.querySelectorAll(".ladiv,.titi"));
+    var nodes = document.querySelectorAll((".ladiv,.titi"));
+
+    var tnodes=Array.prototype.slice.call(nodes,0);
+    tnodes.forEach(function(element, index){
+        element.innerHTML="div **"+index;
+    });
+    console.log(document.querySelector("#premiere"));
+    //pareil que = document.querySelectorAll("#premiere")[0];
+    var deux=document.getElementById("deuxieme");
+    var premier =document.getElementById("premiere");
+    document.getElementById("premiere").addEventListener("click",
+        function(){
+            alert(this.innerHTML);
+        }
+    );
+    document.getElementById("premiere").addEventListener("click",
+        function(){
+            alert(this.innerHTML + " again");
+        }
+    );
+    document.getElementById("premiere").onclick=function(){
+        alert(this.innerHTML);
+    };
+    document.getElementById("deuxieme").onmouseover=function(){
+        alert(this.innerHTML);
+        //document.removeEventListener("mouseover",... ,false);
+        document.getElementById("deuxieme").onmouseover=undefined;
+    };
+
+    deux.style.backgroundColor="yellow";
+    deux.style.width="100px";
+    var par = document.createElement("p");
+    par.innerHTML="Paragraphe";
+    deux.appendChild(par);
+    console.log(deux.childNodes.length);
+    //deux.childNodes[0].remove();
+    deux.removeChild(deux.childNodes[0]);
+};  
 
 /***/ }),
 /* 2 */
