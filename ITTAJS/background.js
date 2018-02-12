d096c2e032cb71aa65ee09a2ff0f86232a496cfd
RubenCardozo <cardozoruben@gmail.com>
2018-02-12 16:16:46 +0100

    Worker et test

--- ITTAJS/background.js
+++ ITTAJS/background.js
@@ -1,1 +1,33 @@
+var compteur = 0;
+var retard= 150;
+
+
+this.onmessage=function(event){
+    console.log(retard);
+    if(event.data.action==="accelerer"){
+        if (retard>10) {
+            retard -=10;
+        }   
+    }else 
+        if(event.data.action==="freiner"){
+            if (retard<1000){
+                retard +=100;
+        }
+    }else 
+        if(event.data.action==="arreter"){
+            this.close();
+    }
+};
+//importScripts('//worker.com/work.js');
+
+
+
+
+function genererValeur(){
+    compteur++;
+    postMessage(compteur);
+    setTimeout(genererValeur, retard);
+}  
+
+genererValeur();
  
